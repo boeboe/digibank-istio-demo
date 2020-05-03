73d9ac75238659953e0902a699c0bb66fdc02385
Bart Van Bos <bartvanbos@gmail.com>
2020-05-03 06:19:52 +0200

    added slack integration in the support service

--- support/server.js
+++ support/server.js
@@ -1,11 +1,9 @@
 'use strict';
 
-require('dotenv').config({silent: true, path: `${__dirname}/.env`});
-
 var server = require('./app');
-var port = 4000;
+var port = process.env.PORT;
 
-console.log(`Running on ${process.env.BASE_PATH}:${port}`)
+console.log(`Running on ${port}`)
 
 server.listen(port, function () {
     console.log('Server running on port: %d', port);
